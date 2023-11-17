b9527b775b6e7aa4ae437e194ebd62e631ceb656
victoriaelizalde <velizalde@udesa.edu.ar>
2023-11-17 13:15:33 -0300

    Cambios con la api

--- js/busqueda.js
+++ js/busqueda.js
@@ -4,31 +4,59 @@ let qsObj = new URLSearchParams(qs);
 let busqueda = qsObj.get("buscador");
 
 
-fetch( `https://api.themoviedb.org/3/search/movie?api_key=${apikey}&query=${busqueda}`)
+
+
+fetch( `https://api.themoviedb.org/3/search/tv?api_key=${apikey}&query=${busqueda}`)
     .then(function (response) {
         return response.json();
     }).then(function (data) {
         console.log(data)
         let series = data.results;
+        console.log(series)
         let contenido = "";
+        let logintud = "";
+        logintud = (data.results).length;
+
+
+
+
+        if (logintud==0){
+            let titulo = document.querySelector("#tituloS");
+            titulo.innerText = "No hay resultado para su búsqueda ";
+            let titulop = document.querySelector("#titulop");
+            titulop.innerText = "";
+       
+        }
+     
         for (let i = 0; i < 5; i++){
    
             contenido += `<article class="index-item">
                             <a href="./detail-serie.html?id=${series[i].id} " class="hipervinculo-index" >
                             <img src="https://image.tmdb.org/t/p/w500/${series[i].poster_path}" alt="Titulo pelicula 5" />
-                            <p class="index-item-title">${series[i].original_title                            }</p>
-                            <p class="index-item-date">Fecha de Estreno: ${series[i].release_date                            }</p>
+                            <p class="index-item-title">${series[i].original_name                            }</p>
+                            <p class="index-item-date">Fecha de Estreno: ${series[i].first_air_date
+                            }</p>
                             </a>
-                        </article>` 
+                        </article>`
         };
+
+
         let indexCard = document.querySelector("#seriesP");
         indexCard.innerHTML = contenido;
+       
+        let titulo = document.querySelector("#tituloS");
+        titulo.innerText = "Resultados series para la busqueda: " + busqueda;
+       
+       
+
+
     }).catch(function (error) {
         console.log(error)
         return error;
     })
 
-    fetch( `https://api.themoviedb.org/3/search/tv?api_key=${apikey}&query=${busqueda}`)
+
+    fetch( `https://api.themoviedb.org/3/search/movie?api_key=${apikey}&query=${busqueda}`)
     .then(function (response) {
         return response.json();
     }).then(function (data) {
@@ -41,19 +69,39 @@ fetch( `https://api.themoviedb.org/3/search/movie?api_key=${apikey}&query=${busq
             contenido += `<article class="index-item">
                             <a href="./detail-movie.html?id=${pelicula[i].id} " class="hipervinculo-index" >
                             <img src="https://image.tmdb.org/t/p/w500/${pelicula[i].poster_path}" alt="Titulo pelicula 5" />
-                            <p class="index-item-title">${pelicula[i].name
+                            <p class="index-item-title">${pelicula[i].title
                             }</p>
-                            <p class="index-item-date">Fecha de Estreno: ${pelicula[i].first_air_date
+                            <p class="index-item-date">Fecha de Estreno: ${pelicula[i].release_date
                             }</p>
                             </a>
-                        </article>` 
+                        </article>`
         };
         let indexCard = document.querySelector("#peliculasP");
         indexCard.innerHTML = contenido;
+
+
+        let titulop = document.querySelector("#titulop");
+        titulop.innerText = "Resultados pelicula para la busqueda: " + busqueda;
+
+
+
+
     }).catch(function (error) {
         console.log(error)
         return error;
     })
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- js/detailserie.js
+++ js/detailserie.js
@@ -5,21 +5,45 @@ let date = document.querySelector(".Detail-item-date");
 let duracion = document.querySelector(".Detail-Movie-Duracion");
 let calificacion = document.querySelector(".calificacion");
 let imagen = document.querySelector(".imagenmovie");
-let generos = document.querySelector(".Detail-Movie-genero");
+
 
 let qs = location.search;
 let qsObj = new URLSearchParams(qs);
 
+
 let id = qsObj.get("id");
+
 
 fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=${apikey}`)
 .then(function (response) {
         return response.json();
 
+
     }).then(function (data) {
 
+
         console.log(data)
-       
+        let generoHTML = "";
+
+
+        for (let i = 0; i < data.genres.length; i++) {
+            genero = data.genres[i].name;
+            idgenero = data.genres[i].id;
+            console.log(idgenero);
+            console.log(genero);
+            generoHTML += `<a href="./detail-genres.html?idgenero=${idgenero}&genero=${genero}&tipo=tv"> ${genero}  </a>`
+
+
+        }
+
+
+        let generosE = document.querySelector("#Detail-Movie-genero");
+        generosE.innerHTML = generoHTML;
+   
+
+
+
+
         titulo.innerText = data.original_name;
         sinopsis.innerText = data.overview;
         date.innerText = data.last_air_date;            ;
@@ -28,10 +52,85 @@ fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=${apikey}`)
         imagen.src = `https://image.tmdb.org/t/p/w500/${data.poster_path}`
         generos.innerHTML = "Generos: " + data.genres[0].name
 
+
     }).catch(function (error) {
         console.log(error)
         return error;
     })
+
+
+
+
+
+
+// RECOMENDACIONES
+
+
+let btn = document.querySelector("#boton");
+btn.addEventListener("click", function(){
+    console.log(id);
+   
+fetch(`https://api.themoviedb.org/3/tv/${id}/recommendations?api_key=${apikey}`)
+.then(function(response){
+    return response.json();
+}).then(function(data){
+    let series = data.results;
+    console.log(series[0])
+
+
+    let contenido = "";
+    for (let i = 0; i < 5; i++){
+
+
+        if (series[i].poster_path!=null) {
+            contenido += `<article class="index-item">
+            <a href="./detail-serie.html?id=${series[i].id} " class="hipervinculo-index" >
+            <img src="https://image.tmdb.org/t/p/w500/${series[i].poster_path}" alt="Titulo pelicula 5" />
+            <p class="index-item-title">${series[i].original_name}</p>
+            <p class="index-item-date">Fecha de Estreno: ${series[i].first_air_date}</p>
+            </a>
+        </article>`
+        }
+       
+ 
+                 
+       
+    };
+
+
+
+
+    let indexCard = document.querySelector("#seriesP");
+    indexCard.innerHTML = contenido;
+
+
+
+
+
+
+
+
+}).catch(function(error){
+    console.log(error)
+    return error;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
 
 
 
